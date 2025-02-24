2bf699a837bf86d00cf6da84ba2bcf28e53dc948
Jan Lewandowski <lewandowski.jan@o2.pl>
2025-02-24 12:22:05 +0100

    feat: display hg on object updates

--- src/nodes/VertexNode.tsx
+++ src/nodes/VertexNode.tsx
@@ -6,15 +6,18 @@ export function VertexNode({ data }: NodeProps<VertexNode>) {
 	return (
 		// We add this class to use the same styles as React Flow's default nodes.
 		<div className="react-flow__node-default">
-			<p>Hash: {data.hash}</p>
-			<p>Node: {data.nodeId}</p>
-			<p>
-				Operation: ({data.operation.type}, {data.operation.value})
-			</p>
+			<div className="vertex-node">
+				<div className="vertex-node__hash">Hash: {data.hash}</div>
+				<div className="vertex-node__id">Node: {data.nodeId}</div>
+				<div className="vertex-node__operation">
+					Operation: {data.operation.type}
+					{data.operation.value && `, ${data.operation.value}`}
+				</div>
+			</div>
 			{data.deps.length > 0 && (
-				<Handle type="target" position={Position.Left} />
+				<Handle type="target" position={Position.Bottom} />
 			)}
-			<Handle type="source" position={Position.Right} />
+			<Handle type="source" position={Position.Top} />
 		</div>
 	);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	build: {
+		target: "esnext",
+	},
+	optimizeDeps: {
+		esbuildOptions: {
+			target: "esnext",
+		},
+	},
+	plugins: [react(), nodePolyfills()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+});
